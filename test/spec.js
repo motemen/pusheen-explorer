67206c679456a481c43942255559b7278368f3a1
motemen <motemen@gmail.com>
2014-03-18 19:21:18 +0900

    added spec

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,4 @@
-describe('pusheen explorer', function () {
+describe('/', function () {
     var entries;
 
     beforeEach(function () {
@@ -10,14 +10,32 @@ describe('pusheen explorer', function () {
         expect(entries.count()).toEqual(50);
     });
 
-    it('should filter by query input', function () {
-        element(by.model('query')).sendKeys('success');
-
-        var imageURLs = element.all(by.repeater('entry in entries')).map(function (el) {
-            return el.findElement(by.css('img')).getAttribute('src');
+    describe('with query input', function () {
+        beforeEach(function () {
+            element(by.model('query')).sendKeys('success');
         });
 
-        expect(imageURLs).toContain('http://25.media.tumblr.com/tumblr_ma3rbnZTE11qhy6c9o7_400.gif');
+        it('should filter by query input', function () {
+            var imageURLs = element.all(by.repeater('entry in entries')).map(function (el) {
+                return el.findElement(by.css('img')).getAttribute('src');
+            });
+
+            expect(imageURLs).toContain('http://25.media.tumblr.com/tumblr_ma3rbnZTE11qhy6c9o7_400.gif');
+        });
+
+        it('should have query string in the location', function () {
+            expect(browser.getCurrentUrl()).toMatch(/\?q=success$/);
+        });
     });
 });
 
+describe('/?q={query}', function () {
+    beforeEach(function () {
+        browser.get('/?q=reality');
+    });
+
+    it('should use ?q as the input query', function () {
+        expect(element(by.model('query')).getAttribute('value')).toEqual('reality');
+    });
+
+})
