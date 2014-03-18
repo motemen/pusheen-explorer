50cd549bdef49be36a0f20140959d4abbcfa2b47
motemen <motemen@gmail.com>
2014-03-18 09:55:21 +0900

    searching

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,33 @@
 var app = angular.module('pusheenExplorer', []);
 
-app.controller('MainCtrl', [ '$scope', '$http', '$timeout',
-    function ($scope, $http, $timeout) {
-        $http.get('data/entries.json').success(function (data) {
-            $scope.entries = data;
-            $scope.imageWidth = 200;
+app.controller('MainCtrl', [ '$scope', '$http', '$timeout', '$filter',
+    function ($scope, $http, $timeout, $filter) {
+        function updateEntries () {
+            var allEntries = $scope.allEntries,
+                query = $scope.query;
+            if (query) {
+                $scope.entries = $filter('filter')(allEntries, { text: query })
+            } else {
+                $scope.entries = _.sample(allEntries, 50);
+                // return $filter('limitTo')($scope.allEntries, 50)
+            }
+
             $timeout(function () {
                 new Masonry(document.querySelector('#photos'));
             });
+        }
+
+        $http.get('data/entries.json').success(function (data) {
+            $scope.allEntries = data;
+            $scope.imageWidth = 200;
+
+            updateEntries();
         });
 
-        $scope.$watch('query', function () {
-            $timeout(function () {
-                new Masonry(document.querySelector('#photos'));
-            });
+        $scope.$watch('query', function (query) {
+            $scope.entries = updateEntries();
+
+            updateEntries();
         });
     }
 ]);
