117241595d4091a2b7df2a9c323f428ee6155058
motemen <motemen@gmail.com>
2014-03-18 19:21:48 +0900

    automatically start test server

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,17 @@
 exports.config = {
   baseUrl: 'http://localhost:8000',
-  specs: ['spec.js']
+
+  onPrepare: function () {
+      var http = require('http'),
+          ecstatic = require('ecstatic');
+      http.createServer(
+          ecstatic({ root: __dirname + '/../app' })
+      ).listen(8000);
+  },
+
+  specs: ['spec.js'],
+
+  jasmineNodeOpts: {
+      isVerbose: true
+  }
 };
