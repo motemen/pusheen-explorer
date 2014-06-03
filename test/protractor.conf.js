ae674a203d3f7b88d044a2fa7c02ce0e8686741c
motemen <motemen@gmail.com>
2014-06-04 00:15:39 +0900

    use firefox for travis

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -3,6 +3,10 @@ var testPort = process.env.TEST_PORT || 9870;
 exports.config = {
   baseUrl: 'http://localhost:' + testPort,
 
+  capabilities: {
+      browserName: 'firefox'
+  },
+
   onPrepare: function () {
       var http = require('http'),
           ecstatic = require('ecstatic');
